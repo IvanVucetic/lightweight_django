27b7be8476b0c92126432ff147bfb1bb4e0639ee
Ivan Vucetic <vuceta87@yahoo.com>
2016-03-25 02:14:41 +0100

    added settings

--- project1/hello.py
+++ project1/hello.py
@@ -1,5 +1,18 @@
+from django.conf import settings
+
 from django.conf.urls import url
 from django.http import HttpResponse
+
+settings.configure(
+	DEBUG=True,
+	SECRET_KEY='thisisthesecretkey', #in production should actually be random
+	ROOT_URLCONF=__name__,
+	MIDDLEWARE_CLASSES=(
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	),
+)
 
 def index(request):
 	return HttpResponse('Hello World!')
@@ -8,5 +21,3 @@ def index(request):
 urlpatterns = (
 	url(r'^$', index),
 )
-
-
