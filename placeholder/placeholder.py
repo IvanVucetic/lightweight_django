3266d5fd05e6c64234cf256df5049c784047f775
Ivan Vucetic <vuceta87@yahoo.com>
2016-03-29 19:58:25 +0200

    Adding Static and Template Settings

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -5,20 +5,30 @@ import sys
 from django.conf import settings
 
 DEBUG = os.environ.get('DEBUG','on') == 'on'
+
 SECRET_KEY = os.environ.get('SECRET_KEY', '^8vc1a3t3umr+gpghe#srsvwlt&=#q7z=u%f-!=^!fzz3bw7@i')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+BASE_DIR = os.path.dirname(__file__)
 
 settings.configure(
 	DEBUG=DEBUG,
 	SECRET_KEY=SECRET_KEY, #in production should actually be random
-	ALLOWED_HOSTS=ALLOWED_HOSTS,
 	ROOT_URLCONF=__name__,
 	MIDDLEWARE_CLASSES=(
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	),
+	INSTALLED_APPS = (
+		'django.contrib.staticfiles',
+	),
+	TEMPLATE_DIRS = (
+		os.path.join(BASE_DIR, 'templates'),
+	),
+	STATICFILES_DIRS = (
+		os.path.join(BASE_DIR, 'static'),
+	),
+	STATIC_URL = '/static/',
 )
 
 #import forms for validation of url parametars
