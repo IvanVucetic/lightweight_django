b6ba1f630979754561e3800291c8d7e59cf5ee0a
Ivan Vucetic <vuceta87@yahoo.com>
2016-04-11 15:11:52 +0200

    started adding meta mechanism

--- chapter3/sitebuilder/views.py
+++ chapter3/sitebuilder/views.py
@@ -1,9 +1,11 @@
+import json
 import os
 
 from django.conf import settings
 from django.http import Http404
 from django.shortcuts import render
-from django.template import Template
+from django.template import Template, Context
+from django.template.loader_tags import BlockNode
 from django.utils._os import safe_join
 
 def get_page_or_404(name):
@@ -19,6 +21,12 @@ def get_page_or_404(name):
     with open(file_path, 'r') as f:
         page = Template(f.read())
 
+    meta = None
+    for i, node in enumerate(list(page.nodelist)):
+        if isinstance(node, BlockNode) and node.name == 'context':
+            meta = page.nodelist.pop(i)
+            break
+    page._meta = meta
     return page
 
 
