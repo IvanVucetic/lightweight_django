ce59c80e30aefe087e8eec2a69b97b476c52a541
Ivan Vucetic <vuceta87@yahoo.com>
2016-04-19 15:52:42 +0200

    views for TaskViewSet and UserViewSet

--- scrum/board/views.py
+++ scrum/board/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import authentication, permissions, viewsets
 
 from .models import Sprint
 from .serializers import SprintSerializer
+
+User = get_user_model()
 
 class DefaultsMixin(object):
     """Default settings for view authentication, permissions,
@@ -24,3 +28,19 @@ class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
     queryset = Sprint.objects.order_by("end")
     serializer_class = SprintSerializer
+
+
+class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating tasks."""
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
+    """API endpoint for listing users."""
+
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
