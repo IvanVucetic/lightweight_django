30dc30cc738fd003ec004297188c668808f6b53d
Ivan Vucetic <vuceta87@yahoo.com>
2016-04-20 22:45:10 +0200

    linking from task to assigned user

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -44,7 +44,18 @@ class TaskSerializer(serializers.ModelSerializer):
         return {
             'self': reverse('task-detail',
                 kwargs={'pk':obj.pk}, request=request),
+            'sprint': None,
+            'assigned': None
         }
+        if obj.sprint_id:
+            links['sprint'] = reverse('sprint-detail',
+                kwargs={'pk': obj.sprint_id}, request=request)
+        if obj.assigned:
+            links['assigned'] = reverse('user-detail',
+                kwargs={User.USERNAME_FIELD: obj.assigned}, required=required)
+        return links
+
+
 
 class UserSerializer(serializers.ModelSerializer):
 
