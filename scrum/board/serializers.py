5aee41d06ddfdb939e61bb5e9b2e117d4975b1c0
Ivan Vucetic <vuceta87@yahoo.com>
2016-04-21 04:15:38 +0200

    fixed syntax - api browser works

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -10,7 +10,7 @@ User = get_user_model()
 
 class SprintSerializer(serializers.ModelSerializer):
 
-    links = serializers.SerializerMethodField('get_links')
+    links = serializers.SerializerMethodField()
 
     class Meta:
         model = Sprint
@@ -29,13 +29,13 @@ class TaskSerializer(serializers.ModelSerializer):
     assigned = serializers.SlugRelatedField(
         slug_field=User.USERNAME_FIELD, required=False,
         allow_null=True, queryset=User.objects.all())
-    status_display = serializers.SerializerMethodField('get_status_display')
-    links = serializers.SerializerMethodField('get_links')
+    status_display = serializers.SerializerMethodField()
+    links = serializers.SerializerMethodField()
 
     class Meta:
         model = Task
-        fields = ('id', 'name', 'description', 'sprint', 'status', 'order',
-            'assigned', 'started', 'due', 'completed', 'links')
+        fields = ('id', 'name', 'description', 'sprint', 'status',
+            'status_display', 'order', 'assigned', 'started', 'due', 'completed', 'links')
 
     def get_status_display(self, obj):
         return obj.get_status_display()
@@ -61,11 +61,11 @@ class TaskSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
 
     full_name = serializers.CharField(source='get_full_name', read_only=True)
-    links = serializers.SerializerMethodField('get_links')
+    links = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active',)
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'links', 'is_active')
 
     def get_links(self, obj):
         request = self.context['request']
