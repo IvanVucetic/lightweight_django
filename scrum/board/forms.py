a953a1220b23e833945b234e3ec38b36f698ef83
Ivan Vucetic <vuceta87@yahoo.com>
2016-04-26 03:40:58 +0200

    finished adding filters

--- scrum/board/forms.py
+++ scrum/board/forms.py
@@ -1,6 +1,10 @@
 import django_filters
 
-from .models import Task
+from django.contrib.auth import get_user_model
+
+from .models import Task, Sprint
+
+User = get_user_model()
 
 class NullFilter(django_filters.BooleanFilter):
     """Filter on a field set as null or not."""
@@ -18,3 +22,18 @@ class TaskFilter(django_filters.FilterSet):
     class Meta:
         model = Task
         fields = ('sprint', 'status', 'assigned', 'backlog',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filters['assigned'].extra.update(
+            {'to_field_name': User.USERNAME_FIELD})
+
+
+class SprintFilter(django_filters.FilterSet):
+
+    end_min = django_filters.DateFilter(name='end', lookup_type='gte')
+    end_max = django_filters.DateFilter(name='end', lookup_type='lte')
+
+    class Meta:
+        model = Sprint
+        fields = ('end_min', 'end_max')
